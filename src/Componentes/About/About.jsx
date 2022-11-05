46c815fa79b5a2f8512e7fc7c6f8e9920c252272
Danie113 <98055160+Danie113@users.noreply.github.com>
2022-11-05 14:17:07 -0500

    change About

--- src/Componentes/About/About.jsx
+++ src/Componentes/About/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 
 function About() {
@@ -22,7 +23,7 @@ function About() {
                                 Preparación y capacitaciones en automatización de procesos con Robots Dispatcher/Performer en Uipath Studio.
                             </p>
                             <div className="edrea_button">
-                                <a href="https://drive.google.com/file/d/1Zj5pVnGZjmuQzxdnySAPGiyNoafYc85j/view?usp=sharing" target= "_blank" >
+                                <a href="https://drive.google.com/file/d/1Zj5pVnGZjmuQzxdnySAPGiyNoafYc85j/view?usp=sharing" target= "_blank" rel="noreferrer" >
                                     Download CV
                                 </a>
                             </div>
